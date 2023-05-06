eb9dea4972334c3850ba81f6f92d6d778e13caab
3panda <3panda@gmail.com>
2023-05-06 18:58:03 +0900

    Modified README.md

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,9 +21,6 @@ require("packer").startup(function(use)
     use{'hrsh7th/nvim-cmp'}
     use{'hrsh7th/cmp-nvim-lsp'}
     use{'hrsh7th/vim-vsnip'}
-    --Flutterサポート　Flutterをneovimで開発したい場合に必要
-    --use {'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim'} 
-    
 end)
 
 -- プラグインを追加したり更新した時は以下のコマンドをコマンドラインモードで実行する
