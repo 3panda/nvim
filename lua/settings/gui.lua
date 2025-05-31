f90cd299c3d0125426baeeec926ceb09839052ac
3panda <3panda@gmail.com>
2025-05-31 14:38:05 +0900

    feat(gui): GUI起動時の初期ディレクトリ設定と⌘Vペースト遅延対策を追加（macOS）

--- lua/settings/gui.lua
+++ lua/settings/gui.lua
@@ -8,3 +8,14 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
   end,
 })
+
+
+-- GUI環境（macOS）でコマンドラインモード中の⌘V（Command + V）ペースト遅延を解消
+
+if vim.fn.has("mac") == 1 and vim.fn.has("gui_running") == 1 then
+  vim.keymap.set('c', '<D-v>', function()
+    vim.schedule(function()
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-r>+', true, false, true), 'n', true)
+    end)
+  end, { noremap = true, silent = true })
+end
